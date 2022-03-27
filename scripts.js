da7023010e74e1e29ecd80773b575d3deaa7abf3
TirumanidasKarthik <karthiktirumanidas@gmail.com>
2022-03-27 12:57:01 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const resumePath = "files/resume.pdf";
+const moreLinkId = document.getElementById("moreLink");
+const moreDropdownContentId = document.getElementById("moreDropdownContent");
+const moreDropdownCaret = document.getElementById("moreDropdownCaret");
+moreLinkId.addEventListener("click", moreLinkDropdown);
+
+function viewResume(){
+    window.open(resumePath);
+}
+ 
+
+
+function moreLinkDropdown(){
+    var x = window.getComputedStyle(moreDropdownContentId).getPropertyValue("display");
+    if(x === "none"){
+        moreDropdownContentId.style.display = "block";
+        moreDropdownCaret.className = "fa fa-caret-left";
+    }else{
+        moreDropdownContentId.style.display = "none";
+        moreDropdownCaret.className = "fa fa-caret-down";
+    }
+}
