86c725a9c0ce7abd58d4daf52828c6c2c88cbb82
TirumanidasKarthik <karthiktirumanidas@gmail.com>
2022-07-30 15:05:25 +0530

    added sliding menu

--- scripts.js
+++ scripts.js
@@ -1,22 +1,5 @@
-const resumePath = "files/myResume.pdf";
-const moreLinkId = document.getElementById("moreLink");
-const moreDropdownContentId = document.getElementById("moreDropdownContent");
-const moreDropdownCaret = document.getElementById("moreDropdownCaret");
-moreLinkId.addEventListener("click", moreLinkDropdown);
-
-function viewResume(){
-    window.open(resumePath);
-}
- 
-
-
-function moreLinkDropdown(){
-    var x = window.getComputedStyle(moreDropdownContentId).getPropertyValue("display");
-    if(x === "none"){
-        moreDropdownContentId.style.display = "block";
-        moreDropdownCaret.className = "fa fa-caret-left";
-    }else{
-        moreDropdownContentId.style.display = "none";
-        moreDropdownCaret.className = "fa fa-caret-down";
-    }
-}
+document.querySelector(".hamburger").addEventListener("click", () => {
+    console.log("clicked");
+    document.querySelector(".slidebar").classList.toggle("active");
+    document.querySelector(".section").classList.toggle("active");
+})
